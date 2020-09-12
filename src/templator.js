58043f3b95b4a813d1b5ea2ed9727a607b153784
Tim Ramsier <timramsier@gmail.com>
2020-09-12 16:56:17 -0700

    [Lesson 4] Add Params Passing

--- src/templator.js
+++ src/templator.js
@@ -47,12 +47,28 @@ function replacePartials(template) {
   const partialRegEx = new RegExp(/{\[[^\[\]]*\]}/g);
   // a regular expression that looks for {}[] brackets only
   const partialBracketsRegEx = new RegExp(/[{\[\]}]/g);
+  const paramsRegEx = new RegExp(/(\w*)\((.*)\)/);
+  const functionRegEx = new RegExp(/(\w+\(.*\)),/g);
 
   // collect all of the partials requests or default to an empty array if none exist
   const partials = html.match(partialRegEx) || [];
   partials.forEach(function (partial) {
     // remove the {[]} brackets from the partial name
-    const fileName = partial.replace(partialBracketsRegEx, "");
+    const partialName = partial.replace(partialBracketsRegEx, "");
+    let fileName = partialName;
+    let params = {};
+    if (paramsRegEx.test(partialName)) {
+      const matches = partialName.match(paramsRegEx);
+      fileName = matches[1];
+      const functionArgs = matches[2].split(functionRegEx);
+      for (let index = 0; index < functionArgs.length; index++) {
+        const argument = functionArgs[index];
+        const argumentMatches = argument.match(paramsRegEx);
+        const key = argumentMatches[1];
+        const value = argumentMatches[2];
+        params[key] = value;
+      }
+    }
     const filePath = path.resolve("templates/" + fileName + ".partial.html");
     const dataPath = path.resolve("data/" + fileName + ".partial.json");
 
@@ -66,7 +82,32 @@ function replacePartials(template) {
       partialData = JSON.parse(rawData);
     }
 
-    html = html.replace(partial, templator(insertion, partialData));
+    html = html.replace(partial, templator(insertion, partialData, params));
+  });
+  return html;
+}
+
+function evaluateCodelets(template, params) {
+  let html = template;
+  const codeletRegEx = new RegExp(/{\%[^\%\%]*\%}/g);
+  const codeletBracketRegEx = new RegExp(/({%|%})/g);
+  const functionNameRegEx = new RegExp(/function (\w*)/);
+  const newLineRegEx = new RegExp(/\n/);
+  const codeletList = template.match(codeletRegEx) || [];
+  const codeList = codeletList.map(function (codelet) {
+    const unbracketedCodelet = codelet.replace(codeletBracketRegEx, "");
+    const noNewLineCodelet = unbracketedCodelet.replace(newLineRegEx, "");
+    return noNewLineCodelet || null;
+  });
+
+  codeletList.forEach(function (codelet, index) {
+    const codeToExecute = codeList[index];
+    const functionName = codeToExecute.match(functionNameRegEx)[1];
+    eval(`var execFn = ${codeToExecute}`);
+    const args = params[functionName] || '';
+    eval(`var argArray = [${args}]`);
+    const result = execFn(...argArray);
+    html = template.replace(codelet, result);
   });
   return html;
 }
@@ -76,7 +117,7 @@ function replacePartials(template) {
  * @param {String} template the untransformed template string
  * @param {Object} pageData a dictionary of what the insertions should be replaced with
  */
-function templator(template, pageData = {}) {
+function templator(template, pageData = {}, params = {}) {
   // default the returned html as the template
   let html = template;
 
@@ -89,6 +130,8 @@ function templator(template, pageData = {}) {
   // by calling replacePartials after insertions are complete it allows
   // for insertions to include partial requests
   html = replacePartials(html);
+
+  html = evaluateCodelets(html, params);
 
   return html;
 }
